0d6d2ed8ae666e7956f0e65f2340b52f98c74cba
Umer Rizwan <43805940+UmerRizwan03@users.noreply.github.com>
2026-01-18 21:05:49 +0530

    Update base URL in sitemap to new domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = "https://example.com"; // REPLACE with your actual domain
+    const baseUrl = "https://umerrizwan.com"; 
 
     return [
         {
